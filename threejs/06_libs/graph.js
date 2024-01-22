adf0d37e34f2f1c171bd260178cbe55af853a1ce
AtelierLibre <36126654+AtelierLibre@users.noreply.github.com>
2024-01-22 15:32:51 +0000

    adds three.js example 06

---
+++ threejs/06_libs/graph.js
@@ -0,0 +1,180 @@
+import { BufferGeometry, Color, Group, Line, LineBasicMaterial, SphereGeometry, MeshBasicMaterial, Mesh} from 'three';
+import { Lut } from 'three/addons/math/Lut.js';
+
+const vertexBasicColorHex = 0x655967;
+const vertexHoverColorHex = 0xFFFFFF;
+const edgeBasicColorHex = 0x989789;
+const edgeLineWidth = 4;
+
+const vertexMesh = new Mesh(
+    new SphereGeometry( 0.1, 16, 16 ),
+    new MeshBasicMaterial({ color:vertexBasicColorHex })
+);
+vertexMesh.userData['basicColorHex'] = vertexBasicColorHex;
+vertexMesh.userData['hoverColorHex'] = vertexHoverColorHex;
+
+const bfsSphereMesh = new Mesh(
+    new SphereGeometry( 0.125, 16, 16 ),
+    new MeshBasicMaterial()
+);
+
+function hashCoordinates(x, y) {
+    var hash = `${x},${y}`;
+    return hash;
+}
+
+export class Vertex {
+    constructor( vertexGroup, rayPlaneIntersection ) {
+        this.vID = hashCoordinates( rayPlaneIntersection.x, rayPlaneIntersection.z );
+        this.neighbours = [];
+        this.mesh = vertexMesh.clone();
+        this.mesh.position.copy( rayPlaneIntersection );
+        this.mesh.userData['vID'] = this.vID;
+        vertexGroup.add( this.mesh )
+    }
+}
+
+export class Graph {
+    constructor( graphObjectsGroup ) {
+        this.adjacencyList = {};
+
+        this.graphObjectsGroup = graphObjectsGroup;
+        this.vertexGroup = graphObjectsGroup.getObjectByName( "vertexGroup" );
+        this.vertexGroup.name = "vertexGroup";
+        this.edgeGroup = graphObjectsGroup.getObjectByName( "edgeGroup" );
+        this.edgeGroup.name = "edgeGroup";        
+
+        this.bfsSphereGroup = new Group();
+        this.graphObjectsGroup.add( this.bfsSphereGroup );
+
+        this.lut = undefined;
+        this.bfsMaterialArray = [];
+    }
+
+    addVertex( rayPlaneIntersection ) {
+        const idHash = hashCoordinates( rayPlaneIntersection.x, rayPlaneIntersection.z );
+        if (!this.adjacencyList[idHash]) {
+            this.adjacencyList[idHash] = new Vertex( this.vertexGroup, rayPlaneIntersection )
+        };
+    }
+
+    addEdge(vID1, vID2) {
+        if ( vID1 !== vID2 ) {
+            if ( this.adjacencyList[vID1]['neighbours'].includes( vID2 ) == false ) {
+                this.adjacencyList[vID1]['neighbours'].push( vID2 );
+                this.adjacencyList[vID2]['neighbours'].push( vID1 );
+
+                // Add representative line to scene
+                const lineGeometry = new BufferGeometry();
+                lineGeometry.setFromPoints ( 
+                    [this.adjacencyList[ vID1 ].mesh.position, this.adjacencyList[ vID2 ].mesh.position]
+                 )
+                /* linewidth on windows will always be 1 */
+                const lineMaterial = new LineBasicMaterial(
+                    { color:edgeBasicColorHex, linewidth:edgeLineWidth }
+                );
+                const line = new Line(lineGeometry, lineMaterial);
+                this.edgeGroup.add(line);
+            };
+        };
+    };
+
+    addbfsSphere( vID, depth ) {
+        const bfsMesh = bfsSphereMesh.clone();
+        bfsMesh.position.copy( this.adjacencyList[ vID ].mesh.position );
+        bfsMesh.material = this.bfsMaterialArray[depth];
+        this.bfsSphereGroup.add( bfsMesh )
+    }
+
+    bfs(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        visited[start] = true;
+
+        while (queue.length) {
+            let currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex]['neighbours'].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+
+        return result;
+    };
+
+    bfs_depth(start) {
+        const queue = [{ vertex: start, depth: 0 }];
+        const result = [];
+        const visited = {};
+        visited[start] = true;
+    
+        while (queue.length) {
+            let current = queue.shift();
+            let currentVertex = current.vertex;
+            let currentDepth = current.depth;
+            result.push({ vertex: currentVertex, depth: currentDepth });
+    
+            this.adjacencyList[currentVertex]['neighbours'].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push({ vertex: neighbor, depth: currentDepth + 1 });
+                }
+            });
+        }
+    
+        return result;
+    }
+
+    traverseGraph( start, limit ) {
+
+        // Empty children
+        this.bfsSphereGroup.children.length = 0;
+        this.bfsMaterialArray.length = 0;
+
+        // Create a new color lookup table with the same
+        // number of colors as the selected limit
+        this.lut = new Lut( 'rainbow', limit );
+        this.bfsMaterialArray = this.lut.lut.map(
+            (c) => new MeshBasicMaterial({ color: c })
+        ).reverse();
+
+        const result = this.bfs_depth_limit( start, limit );
+        return result;
+
+    }
+
+    bfs_depth_limit(start, limit) {
+
+        const queue = [{ vertex: start, depth: 0 }];
+        const result = [];
+        const visited = {};
+        visited[start] = true;
+    
+        while (queue.length) {
+            let current = queue.shift();
+            let currentVertex = current.vertex;
+            let currentDepth = current.depth;
+            result.push({ vertex: currentVertex, depth: currentDepth });
+    
+            // Insert to get spheres to appear as algorithm progresses
+            this.addbfsSphere( currentVertex, currentDepth );
+            //
+
+            if (currentDepth < limit) {
+                this.adjacencyList[currentVertex]['neighbours'].forEach(neighbor => {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.push({ vertex: neighbor, depth: currentDepth + 1 });
+                    }
+                });
+            }
+        }
+    
+        return result;
+    };
+};
---
+++ threejs/06_main.js
@@ -0,0 +1,210 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { Graph } from './06_libs/graph.js';
+
+// Scene, camera, renderer
+//////////////////////////
+const canvasRef = document.querySelector('canvas.threejsCanvas');
+let scene, camera, renderer, controls;
+let sphereInter, raycaster, pointer, cube, plane;
+let rayPlaneIntersection = new THREE.Vector3();
+let rayVertexIntersection;
+let vID1, vID2;
+let rayIntersectedObjects;
+let intersectedObject = null;
+let mouseDown = false;
+let mouseMoved = false;
+const bfsSettings = {
+    steps:5,
+};
+
+const vertexGroup = new THREE.Group();
+vertexGroup.name = "vertexGroup";
+const edgeGroup = new THREE.Group();
+edgeGroup.name = "edgeGroup";
+let graphObjectsGroup = new THREE.Group(); // ThreeJS Group for vertices and edges, gets added to scene
+graphObjectsGroup.add( vertexGroup );
+graphObjectsGroup.add( edgeGroup );
+
+let graph; // The graph object
+
+function init() {
+    // initial setup - everything you want to run once, before the animation starts.
+
+    // Scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0, 0, 0 ); // Black background
+
+    // Camera
+    camera = new THREE.PerspectiveCamera(
+        75, // Field of view
+        window.innerWidth / window.innerHeight, // Aspect ratio
+        0.1, // Near clipping plane
+        1000 // Far clipping plane
+    );
+    camera.position.z = 5;
+    camera.position.y = 5;
+    scene.add( camera );
+
+    // Grid helper
+    const gridHelper = new THREE.GridHelper(
+        100,
+        100,
+        0x888888,
+        0x444444
+        );
+    scene.add( gridHelper );
+
+    // Axes helper
+    const axesHelper = new THREE.AxesHelper( 5 );
+    scene.add( axesHelper );
+
+    // Renderer
+    renderer = new THREE.WebGLRenderer({ canvas:canvasRef, antialiasing:true });
+    renderer.setPixelRatio( window.devicePixelRatio * 1.5 );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    // Controls
+    controls = new OrbitControls( camera, renderer.domElement )
+
+    // GUI
+    const gui = new GUI();
+    gui.add(bfsSettings, 'steps');
+
+    // Raycaster and pointer
+    // Create a Plane object
+    plane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0); // Adjust the normal and constant as needed
+    pointer = new THREE.Vector2();
+    raycaster = new THREE.Raycaster();
+    // Sphere to show where pointer intersects with object
+    const sphereInterGeometry = new THREE.SphereGeometry( 0.1 );
+    const sphereInterMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    sphereInter = new THREE.Mesh( sphereInterGeometry, sphereInterMaterial );
+    sphereInter.visible = false;
+    scene.add( sphereInter );
+
+    // Graph
+    graph = new Graph( graphObjectsGroup );
+    scene.add( graphObjectsGroup );
+
+    window.addEventListener( 'resize', onWindowResize );
+    window.addEventListener( 'mousedown', onMouseDown );
+    window.addEventListener( 'mousemove', onMouseMove );
+    window.addEventListener( 'mouseup', onMouseUp );
+
+    render();
+};
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+};
+
+function onMouseDown() {
+    mouseDown = true;
+    mouseMoved = false;
+
+    if ( rayVertexIntersection ) {
+        controls.enabled = false;//! event.value;
+        vID1 = rayIntersectedObjects[0].object.userData['vID'];
+    }
+}
+
+function onMouseMove( event ) {
+
+    mouseMoved = true;
+
+    // calculate pointer position in normalized device coordinates (-1 to +1)
+    pointer.x = ( event.clientX / canvasRef.clientWidth ) * 2 - 1;
+    pointer.y = - ( event.clientY / canvasRef.clientHeight ) * 2 + 1;
+
+    if (mouseDown && (vID1 !== null)) {
+        console.log('show a temporary edge...')
+    }
+};
+
+function onMouseUp ( event ) {
+    if (mouseDown && !mouseMoved) {
+        if ( !rayVertexIntersection ) {
+            graph.addVertex( rayPlaneIntersection );
+        } else {
+            //console.log(
+                graph.traverseGraph( rayIntersectedObjects[0].object.userData['vID'], bfsSettings.steps )
+            //)
+        }
+    } else if (mouseDown && mouseMoved) {
+        if ( rayVertexIntersection ) {
+            vID2 = rayIntersectedObjects[0].object.userData['vID'];
+            graph.addEdge( vID1,vID2 );
+        }
+    }
+
+    // Reset the flags
+    mouseDown = false;
+    mouseMoved = false;
+    controls.enabled = true;
+    vID1 = null;
+    vID2 = null;
+}
+
+const vertexOriginalMaterial = new THREE.MeshBasicMaterial({color: 0x655967});
+const vertexHoverMaterial = new THREE.MeshBasicMaterial({color: 0xFFFF00});
+
+// Render
+/////////
+function render() {
+
+    // update the raycaster
+    raycaster.setFromCamera( pointer, camera );
+
+    // intersect raycaster with plane
+    raycaster.ray.intersectPlane( plane, rayPlaneIntersection );
+    if ( rayPlaneIntersection !== null ) {
+        sphereInter.visible = true;
+        sphereInter.position.copy( rayPlaneIntersection );
+    } else {
+        sphereInter.visible = false;
+    };
+
+    // intersect raycaster with graph objects
+    rayIntersectedObjects = raycaster.intersectObjects(
+        vertexGroup.children
+        );
+
+    // if there are intersected objects
+    if ( rayIntersectedObjects.length > 0 ) {
+        rayVertexIntersection = true;
+        // if intersectedObject is different from the current intersected object
+        if ( intersectedObject != rayIntersectedObjects[0].object ) {
+            // reset the current (old) intersectedObject's color
+            if ( intersectedObject ) {
+                intersectedObject.material = vertexOriginalMaterial;
+            }
+
+            // Update intersectedObject to the new object & change its color
+            intersectedObject = rayIntersectedObjects[0].object;
+            intersectedObject.material = vertexHoverMaterial;
+        }
+
+    // if the ray doesn't intersect any objects
+    } else {
+        rayVertexIntersection = false;
+        // If intersectedObject still references an object, reset the selection map materials
+        if ( intersectedObject ) {
+            // reset the current (old) intersectedObject's color
+            intersectedObject.material = vertexOriginalMaterial;//intersectedObject.userData['originalColor'];
+        };
+        // Set intersectedObject to reference null
+        intersectedObject = null;
+
+    };
+
+    controls.update()
+
+    renderer.render( scene, camera );
+    requestAnimationFrame(render);
+};
+
+init();
